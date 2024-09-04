ae26e0bf109ab16792894731bca0cb9c15a3ff47
WavyCraftNetwork <42108413+WavyCraftNetwork@users.noreply.github.com>
2024-09-04 01:30:50 -0700

    Fixed

--- src/BlockHorizons/Fireworks/FireworkManager.php
+++ src/BlockHorizons/Fireworks/FireworkManager.php
@@ -11,28 +11,30 @@ use pocketmine\utils\SingletonTrait;
 use pocketmine\player\Player;
 use pocketmine\world\World;
 use pocketmine\math\Vector3;
-use pocketmine\world\Position;
+use pocketmine\entity\Location;
 
 class FireworkManager
 {
     use SingletonTrait;
 
-    public function spawnFireworkAtPlayer(Player $player, int $type, string $color, string $fade = "", int $duration = 1, bool $flicker = false, bool $trail = false): void
-    {
-        $this->spawnFirework($player->getPosition()->getX(), $player->getPosition()->getY(), $player->getPosition()->getZ(), $player->getWorld(), $type, $color, $fade, $duration, $flicker, $trail);
-    }
-
-    public function spawnFireworkAtCoord(int $type, string $color, string $fade = "", int $duration = 1, bool $flicker = false, bool $trail = false, float $x, float $y, float $z, World $world): void
-    {
-        $this->spawnFirework($type, $color, $fade, $duration, $flicker, $trail, $x, $y, $z, $world);
-    }
-
-    private function spawnFirework(float $x, float $y, float $z, World $world, int $type, string $color, string $fade = "", int $duration = 1, bool $flicker = false, bool $trail = false): void
+    public function spawnFireworkAtPlayer(int $type, string $color, string $fade = "", int $duration = 1, bool $flicker = false, bool $trail = false, Player $player): void
     {
         $fireworkItem = ExtraVanillaItems::FIREWORKS();
         $fireworkItem->setFlightDuration($duration);
         $fireworkItem->addExplosion($type, $color, $fade, $flicker, $trail);
-        $position = new Position($x, $y, $z, $world);
+
+        $position = $player->getLocation();
+        $fireworkRocket = new FireworksRocket($position, $fireworkItem);
+        $fireworkRocket->spawnToAll();
+    }
+
+    public function spawnFireworkAtCoord(int $type, string $color, string $fade = "", int $duration = 1, bool $flicker = false, bool $trail = false, float $x, float $y, float $z, World $world): void
+    {
+        $fireworkItem = ExtraVanillaItems::FIREWORKS();
+        $fireworkItem->setFlightDuration($duration);
+        $fireworkItem->addExplosion($type, $color, $fade, $flicker, $trail);
+
+        $position = new Location($x, $y, $z, $world, 0.0, 0.0);
         $fireworkRocket = new FireworksRocket($position, $fireworkItem);
         $fireworkRocket->spawnToAll();
     }
